013fff86ae50767f57d7175bfe6302177e6962d4
liujf <liujf14@ziroom.com>
2023-10-12 09:42:23 +0800

    update

--- internal/pkg/core/core.go
+++ internal/pkg/core/core.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-gin-web/internal/color"
+	"math/rand"
+	"strconv"
 )
 
 const _UI = `
@@ -23,9 +25,12 @@ type mux struct {
 func New() (*gin.Engine, error) {
 	fmt.Println(color.Blue(_UI))
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/user/ping", func(c *gin.Context) {
+		data := "pong" + strconv.Itoa(rand.Int())
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "success",
+			"code":    200,
+			"data":    data,
 		})
 	})
 	return r, nil
